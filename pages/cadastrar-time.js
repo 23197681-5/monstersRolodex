66ed23116da206a5f139811881c92aab453c47c4
Willian <willianlauber@gmail.com>
2025-11-19 20:10:06 -0400

    supabase folder

--- pages/cadastrar-time.js
+++ pages/cadastrar-time.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../src/supabaseClient';
+import { supabase } from '../src/lib/supabaseClient';
 
 const heavenlyStems = [
   { value: '甲', label: '甲 (jiǎ) - Madeira Yang' },
